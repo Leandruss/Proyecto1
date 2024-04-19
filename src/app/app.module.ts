4c8163d81e83addaffbec03e35b5446ace3d5f5b
Leandruss <carrascoariel001@gmail.com>
2024-04-19 11:04:02 -0300

    hola

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { CarruselComponent } from './carrusel/carrusel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    GaleriaComponent,
+    CarruselComponent
   ],
   imports: [
     BrowserModule,
